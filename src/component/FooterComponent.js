47d3c13128a58f044ec0ccdbe4b278b31a20bb51
Asis Sotelo <54049819+AsisASotelo@users.noreply.github.com>
2020-05-11 11:06:02 -0700

    Add Footer

---
+++ src/component/FooterComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import Fade from 'react-reveal/Fade'
+
+
+
+function Footer(props){
+    return(
+       
+        <div className="footer">
+        <div className="container">
+                <div className="row justify-content-center">             
+                
+                </div>
+                <div className="row justify-content-center footercustom">             
+                    <div className="col-auto">
+                        
+                        <p>Designed in Los Angeles, California</p>
+                        <p>© Copyright 2020 Asis Sotelo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+
+
+    );
+}
+
+export default Footer
