704e405a6295deb8b8b2badd37149587a47fb652
AsisASotelo <asis.a.sotelo@gmail.comm>
2020-05-11 13:10:21 -0700

    Remove Footers

--- src/component/ProjectComponent.js
+++ src/component/ProjectComponent.js
@@ -100,7 +100,7 @@ class Project extends Component {
                                         <CardBody>
                                             <CardImg width ="100%" src="/assets/images/mandelbulb.png" alt="mandelbulb.png"></CardImg>
                                             <hr></hr>
-                                            <CardText>Creates an image of a Mandelbulb which is a 3d extension of the 2-d Mandelbrot set. The image is 500 by 500 pixels and implements distance estimatation for the ray tracing allowing some shadowing to appear in the image. The entirety of the code was written in python and implements standard modules. It is quite lengthy and you have to change the source code to get different types of images. The time to render is ~ 3-4 minutes on the rasipberry pi model 3B+ I ran it on. </CardText>
+                                            <CardText>Creates an image of a Mandelbulb which is a 3d extension of the 2-d Mandelbrot set. The image is 500 by 500 pixels and implements distance estimation for the ray tracing allowing some shadowing to appear in the image. The entirety of the code was written in python and implements standard modules. It is quite lengthy and you have to change the source code to get different types of images. The time to render is ~ 3-4 minutes on the rasipberry pi model 3B+ I ran it on. </CardText>
                                             <hr></hr>
                                                     <ButtonGroup>
                                                         <Button> <Icon icon={pythonIcon} />  Python </Button>
