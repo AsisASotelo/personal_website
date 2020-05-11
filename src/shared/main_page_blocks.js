fb53ac379ff85f168d9a95aafc0c813990f50780
Asis Sotelo <54049819+AsisASotelo@users.noreply.github.com>
2020-05-11 11:00:06 -0700

    Add files via upload

---
+++ src/shared/main_page_blocks.js
@@ -0,0 +1,14 @@
+export const MAIN_BLOCKS=
+[
+    {
+        id: 0,
+        dishId: 0,
+        rating: 5,
+        comment: "I am Asis Sotelo, and this is my website. I enjoy simple design aesthetics so I modeled this after the website created by Jeremy Sallee. This website is relatively brand new but it is as good a starting point as any. I love solving problems and using computers. I love eating and making food. I graduated from University of California, Santa Barbara (Go Gauchos) with a degree in Physics. This page acts as a landing page that stores all pertinent information in my life. Throughout this website you can find links to projects, a blog (which still needs to be created), and hopefully more. You may contact me if you have any questions, feedback, say hello, by clicking my contact page. ",
+        author: "About Me",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+   
+
+    
+]
