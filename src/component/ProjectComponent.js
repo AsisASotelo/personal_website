3b3ed8c26c91d254bc7d2a5187373568e00c9dcb
AsisASotelo <asis.a.sotelo@gmail.comm>
2020-05-12 13:38:02 -0700

    Add More Projects

--- src/component/ProjectComponent.js
+++ src/component/ProjectComponent.js
@@ -11,6 +11,7 @@ import reactIcon from '@iconify/icons-logos/react';
 import foursquareIcon from '@iconify/icons-fa-brands/foursquare';
 import linuxIcon from '@iconify/icons-fa-brands/linux';
 import latexIcon from '@iconify/icons-simple-icons/latex';
+import hadoopIcon from'@iconify/icons-logos/hadoop';
 
 
 
@@ -103,7 +104,7 @@ class Project extends Component {
                                             <CardBody>
                                                 <CardImg width ="100%" height="360px" src="/assets/images/mandelbulb.png" alt="mandelbulb.png"></CardImg>
                                                 <hr></hr>
-                                                <CardText>Creates an image of a Mandelbulb which is a 3d extension of the 2-d Mandelbrot set. The image is 500 by 500 pixels and implements distance estimation for the ray tracing allowing some shadowing to appear in the image. </CardText>
+                                                <CardText>Creates an image of a Mandelbulb which is a 3d extension of the 2-d Mandelbrot set. The image is 500 by 500 pixels and implements distance estimation for the ray tracing allowing some shadowing to appear in the image. Computational complexity causes a loading time of 3 minutes on average. All of the work was done on a Raspberry PI 3B+. </CardText>
                                                 <hr></hr>
                                                         
                                                             <Button color="info" > <Icon icon={pythonIcon} />  Python </Button>{' '}
@@ -124,7 +125,7 @@ class Project extends Component {
                                         <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} >
                                             <CardHeader tag="h3">Fourier Analysis</CardHeader>
                                                 <CardBody>
-                                                    <CardImg width ="100%" height="50%" src="/assets/images/fourier.png" alt="firebase.png"></CardImg>
+                                                    <CardImg width ="100%" height="360px" src="/assets/images/fourier.png" alt="firebase.png"></CardImg>
                                                     <hr></hr>
                                                     <CardText>Scientific report on the removal of noise from a noise time series utilizing Fourier Analysis. Most of the work was done using Jupyter Notebook and took in stock data over 2 years. Using FFT algorithm was able to decrease the noise and isolate the true signal frequency of the underlying data. Report created with LaTeX. </CardText>
                                                     <hr></hr>
@@ -139,6 +140,47 @@ class Project extends Component {
                                         </Card>  
                                     </div>                                
                                 </div>
+                                <div xs ="12" class="col-md-6 col-12">
+                                    <div class="mx-auto mt-5">  
+                                        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} >
+                                            <CardHeader tag="h3">HDFS Inverter</CardHeader>
+                                                <CardBody>
+                                                    <CardImg width ="100%" height="360px" src="/assets/images/inverter.png" alt="firebase.png"></CardImg>
+                                                    <hr></hr>
+                                                    <CardText> Analyzed the Hadoop Distributed file system I tokenized the file and directory names of a Hadoop cluster Name Node deployed on Amazon Linux EC2 instances. I then wrote a python script to take an xml file and then produce another xml file known as an inverted index. Listing the file/directory names inode numbers. </CardText>
+                                                    <hr></hr>
+                                                    
+                                                        <Button color="info"> <Icon icon={pythonIcon} />  Python </Button>{' '}
+                                                        <Button color="primary"> <Icon icon={hadoopIcon} /> Hadoop </Button>
+                                                    
+                                                    <hr></hr>
+                                                    <a href="https://github.com/AsisASotelo/Fourier_Transform_Project"><Button><FontAwesomeIcon icon={faGithub} size="lg"/> View On GitHub</Button></a>
+                                                </CardBody>
+                                            
+                                        </Card>  
+                                    </div>                                
+                                </div>
+                                <div xs ="12" class="col-md-6 col-12">
+                                    <div class="mx-auto mt-5">  
+                                        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} >
+                                            <CardHeader tag="h3">Personal Website</CardHeader>
+                                                <CardBody>
+                                                    <CardImg width ="100%" height="360px%" src="/assets/images/website.png" alt="firebase.png"></CardImg>
+                                                    <hr></hr>
+                                                    <CardText>The personal website which you are on! Designed utilizing ReactJs and Bootstrap. Great care was taken to get the aesthetics just right. The component based architecture allows for the quick swapping and rearrangement of website elements. I am constantly updating this website, as I have become obsessed with its look. </CardText>
+                                                    <hr></hr>
+                                                    
+                                                        <Button color="danger"> <Icon icon={reactIcon} />  React </Button>{' '}
+                                                        <Button color="primary"> <Icon icon={latexIcon} /> LaTeX </Button>
+                                                    
+                                                    <hr></hr>
+                                                    <a href="https://github.com/AsisASotelo/Fourier_Transform_Project"><Button><FontAwesomeIcon icon={faGithub} size="lg"/> View On GitHub</Button></a>
+                                                </CardBody>
+                                            
+                                        </Card>  
+                                    </div>                                
+                                </div>
+
                                 
                             </div>
 
